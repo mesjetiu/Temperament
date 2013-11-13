50522a2f8634b911788a52f71591111318af2dd8
Carlos Arturo Guerra Parra <carlosarturoguerra@gmail.com>
2013-11-13 22:53:00 +0100

    creado modulo y main.py. Añadidas más constantes

--- temperamentos.py
+++ temperamentos.py
@@ -1,19 +1,32 @@
-# -*- coding: utf-8 -*-
-
+# Modulo "temperamentos"
 # Conjunto de funciones utiles para afinaciones y temperamentos
 
 import math
 
+# Constantes
+quinta_pitag = 3.0/2
+q = quinta_pitag
+
+tercera_justa = 5.0/4
+t = tercera_justa
+
+octava = 2
+o = octava
+
 coma_pitag = (3.0/2)**12 / 2**7
+cp = coma_pitag
 
 coma_sint = (3.0/2)**4 / (5.0/4)**4
+cs = coma_sint
 
 diesis = coma_sint**3 / coma_pitag
+die = diesis
 
 fi = (math.sqrt (5) + 1) / 2
 
-def to_cents (inter):
+def cents (inter):
     return math.log(inter, math.pow (2.0, (1.0 / 1200)))
+
 
 def pitch_pipe (pitch, temp_ideal, temp):
     return (pitch * math.sqrt (1.4 * 8.314 * (273.15 + temp) /
@@ -24,6 +37,7 @@ def pitch_pipe (pitch, temp_ideal, temp):
 
 def divide_inter (a, b, n_divisiones, division):
 	return float(a) / math.pow(float(a)/b,1.0/n_divisiones)**division
+
 
 
 
