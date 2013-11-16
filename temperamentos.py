7bdba117207b2629a0bda7772278872286fffb3e
Carlos Arturo Guerra Parra <carlosarturoguerra@gmail.com>
2013-11-16 02:40:34 +0100

    quinta del lobo calculada

--- temperamentos.py
+++ temperamentos.py
@@ -21,22 +21,21 @@ def divide_inter (a, b, n_divisiones, division):
 # Constantes
 
 quinta_pitag = 3.0/2
-q = cents(quinta_pitag)
+q = round(cents(quinta_pitag),4)
 
 tercera_justa = 5.0/4
-t = cents(tercera_justa)
-
+t = round(cents(tercera_justa),4)
 octava = 2
-o = cents(octava)
+o = round(cents(octava),4)
 
 coma_pitag = (3.0/2)**12 / 2**7
-cp = cents(coma_pitag)
+cp = round(cents(coma_pitag),4)
 
 coma_sint = (3.0/2)**4 / 2**2 / (5.0/4)
-cs = cents(coma_sint)
+cs = round(cents(coma_sint),4)
 
 diesis = coma_sint**3 / coma_pitag
-die = cents(diesis)
+die = round(cents(diesis),4)
 
 L = 'lobo'
 
@@ -54,21 +53,27 @@ def T12_cents (_quintas, redondeo = 2):
 		if _quintas[i] == L:
 			_T12.append ('lobo')
 			continue
-		_suma = (_suma + q + _quintas[i]) % 1200
+		_suma = (_suma + q + _quintas[i])
 		_T12.append (_suma)	
 		
-	#Calculo de la quinta de lobo si la hay
+	# Calculo de la quinta de lobo si la hay
 	if 'lobo' in _T12:
-		lobo = _T12[-1]
+		lobo = _T12[-1] + q - o*7
 		print "lobo =", lobo
 		punto_lobo = _T12.index('lobo')
-		_T12[punto_lobo] = lobo
+		_T12[punto_lobo] = _T12[punto_lobo-1] + q - lobo
 		for i in range(punto_lobo+1, len(_T12)):
-			_T12[i] = _T12[i] + lobo
+			_T12[i] = _T12[i] + q - lobo
 	
+	# Reduccion a una octava
+	for i in range(len(_T12)):
+	
+		_T12[i] = _T12[i] % 1200
+	
+	# Ordenacion por semitonos
 	_T12_orden = [_T12[6], _T12[1],  _T12[8], _T12[3], _T12[10], _T12[5], _T12[0], _T12[7], _T12[2], _T12[9], _T12[4], _T12[11]]
 	
-	for i in range(12):
+	for i in range(len(_T12_orden)):
 		_T12_orden[i] = _T12_orden[i] - ((i+1)*100)%1200
 
 	_T12_orden.insert(0, _T12_orden[-1])
@@ -95,4 +100,5 @@ pit = [-cp,0,0,0, 0,0,0,0, 0,0,0,0]
 
 c = cs/4
 mesotonico = [ -c,-c,-c,-c, -c,-c,-c,-c, L,-c,-c,-c ]
+#mesotonico = [ -c,-c,-c,-c, -c,-c,-c,-c, -cp+3*cs-c,-c,-c,-c ]
 T12_cents (mesotonico)
